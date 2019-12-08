1614da054d1b37407683e479c636cd2457598b7d
dipingarg999 <49325687+dipingarg999@users.noreply.github.com>
2019-12-08 16:19:53 +0530

    Demo

---
+++ Demo.java
@@ -0,0 +1,16 @@
+package introduction;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		
+
+
+	    driver.get("http://www.google.com");
+	    System.out.println("title of page is: " + driver.getTitle());
+	}
+
+}
